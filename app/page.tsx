c77b3f357fdb5d6946a96ce2d59db6e01a4f7108
tian <istiancz@gmail.com>
2024-09-25 19:02:21 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,19 @@ import { imgs } from './imgs'
 export default function Home() {
 	return (
 		<div className='flex flex-col min-h-screen p-8 pb-20 gap-16 sm:p-10 font-[family-name:var(--font-geist-sans)]'>
-			<Image
-				src='https://panels.art/images/Group-8187.svg'
-				width={200}
-				height={50}
-				alt='logo'
-			/>
+			<div className='relative w-[200px] h-[80px]'>
+				<Image
+					src='https://panels.art/images/Group-8187.svg'
+					width={200}
+					height={50}
+					alt='logo'
+					className='block'
+				/>
+				<div className='absolute left-12 inset-0 flex items-center justify-center'>
+					<div className='w-full h-2 bg-red-500 rotate-45 translate-x-1/2'></div>
+					<div className='w-full h-2 bg-red-500 -rotate-45 -translate-x-1/2'></div>
+				</div>
+			</div>
 			<main className='flex flex-col items-center gap-8'>
 				<h1 className='text-4xl font-bold max-w-2xl text-center'>
 					Transform your screen into an art gallery with curated wallpapers
@@ -37,7 +44,13 @@ export default function Home() {
 							{Object.entries(collection).map(([key, url]) => (
 								<div key={key}>
 									<p>{key}</p>
-									<Image src={url} width={300} height={100} alt='wallpaper' />
+									<Image
+										className='h-auto w-auto'
+										src={url}
+										width={300}
+										height={1000}
+										alt='wallpaper'
+									/>
 								</div>
 							))}
 						</div>
