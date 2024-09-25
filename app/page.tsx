d8c6430659aca84588aa343bee036cf4c4c7b9ca
tian <istiancz@gmail.com>
2024-09-25 19:24:28 +0800

    add animation

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,32 @@ import { imgs } from './imgs'
 export default function Home() {
 	return (
 		<div className='flex flex-col min-h-screen p-8 pb-20 gap-16 sm:p-10 font-[family-name:var(--font-geist-sans)]'>
+			<div
+				data-w-id='d590085d-0897-06e6-1778-ccc9c1bbcf4c'
+				className='animate-translateHero z-[-1] filter blur-[500px] bg-transparent flex flex-row w-[30%] h-[75%] absolute inset-[20rem_0_0_auto] '
+				style={{
+					transform:
+						'translate3d(-20rem, 0rem, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
+					transformStyle: 'preserve-3d',
+				}}
+			>
+				<div
+					data-w-id='988fce89-75ac-45fc-0950-0097f923818f'
+					className='w-full h-full'
+					style={{ backgroundColor: 'rgb(223, 189, 139)' }}
+				></div>
+				<div
+					data-w-id='c5b9f9d1-d41e-a371-4bc6-0e8e042e431f'
+					className='w-full h-full'
+					style={{ backgroundColor: 'rgb(253, 159, 80)' }}
+				></div>
+				<div
+					data-w-id='4789fc1a-191d-5ebb-d75e-240b1df852d8'
+					className='w-full h-full'
+					style={{ backgroundColor: 'rgb(218, 53, 25)' }}
+				></div>
+			</div>
+
 			<div className='relative w-[200px] h-[80px]'>
 				<Image
 					src='https://panels.art/images/Group-8187.svg'
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+			keyframes: {
+				translateHero: {
+					'0%': {
+						transform: 'translate3d(-20rem, 0rem, 0px)',
+					},
+					'50%': {
+						transform: 'translate3d(-60rem, -29rem, 0px)',
+					},
+					'100%': {
+						transform: 'translate3d(-20rem, 0rem, 0px)',
+					},
+				},
+			},
+			animation: {
+				translateHero: 'translateHero 15s ease-in-out infinite', // You can customize the duration, timing function, and repeat
+			},
+		},
+	},
+	plugins: [],
+}
+export default config
